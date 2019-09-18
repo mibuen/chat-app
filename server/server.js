43ceaf15b356dd4059579fb43338417e638d59f7
Manuel Segovia <msegovia@mac.com>
2019-09-18 14:50:40 -0500

    change app.listen

--- server/server.js
+++ server/server.js
@@ -3,11 +3,11 @@ const path = require('path');
 const express = require('express');
 
 const publicPath = path.join(__dirname,'../public');
-const port = process.env.PORT;
+const port = process.env.POR || 3000;
 const app = express();
 
 app.use(express.static(publicPath));
 
-app.listen(3000, ()=>{
+app.listen(port, ()=>{
   console.log(`Server is up on ${port}`);
 });
