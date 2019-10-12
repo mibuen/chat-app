9121fc02f27f254ea9f193d1b86add2732e42fa4
Manuel Segovia <msegovia@mac.com>
2019-10-12 12:05:40 -0400

    added io module for connect and disconnect server

--- server/server.js
+++ server/server.js
@@ -1,13 +1,28 @@
 
 const path = require('path');
+const http = require('http');
 const express = require('express');
+const socketIO = require('socket.io');
+
+
+
 
 const publicPath = path.join(__dirname,'../public');
 const port = process.env.PORT || 3000;
 const app = express();
+const server = http.createServer(app);
+const io = socketIO(server);
 
 app.use(express.static(publicPath));
 
-app.listen(port, ()=>{
+io.on('connection',(socket)=>{
+  console.log('New User Connected');
+  socket.on('disconnect',()=>{
+    console.log('CLIENTE DESCONECTADO');
+  });
+});
+
+
+server.listen(port, ()=>{
   console.log(`Server is up on ${port}`);
 });
