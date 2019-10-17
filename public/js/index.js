af9f8783ec2e6de76c82f40039fbdcce68324496
Manuel Segovia <msegovia@mac.com>
2019-10-17 07:02:23 -0400

    refactored server and index

--- public/js/index.js
+++ public/js/index.js
@@ -1,21 +1,16 @@
 
-
     const socket = io();
+
     socket.on('connect',()=>{
       console.log('CONNECTED TO SERVER');
-    socket.emit('createMessage', {
-        from: 'ChiQuitoKBrown',
-        text: 'This is Working for ChiQuito'
-      });
-    
-    });
-    socket.on('disconnect',()=>{
-      console.log('DESCONECTADO');
-    });
-  socket.on('newMessage',(message)=>{
-   console.log('newMessage',message);
+    });  
+    socket.on('newMessage',(message)=>{
+      console.log('New Message',message);
   });
 
+  socket.on('disconnect',()=>{
+    console.log('DESCONECTADO');
+  });
 
 
 
--- server/server.js
+++ server/server.js
@@ -4,9 +4,6 @@ const http = require('http');
 const express = require('express');
 const socketIO = require('socket.io');
 
-
-
-
 const publicPath = path.join(__dirname,'../public');
 const port = process.env.PORT || 3000;
 const app = express();
@@ -16,16 +13,19 @@ const io = socketIO(server);
 app.use(express.static(publicPath));
 
 io.on('connection',(socket)=>{
-  console.log('New USER SUPER_KonecTed al Servidor');
+  console.log('New USER Connected');
+
   socket.emit('newMessage', {
-    from: 'user@kbrown.com',
-    text: 'This is el Chiquito57',
+    from: 'Admin@STARB',
+    text: 'Welcome to the  Chat App.',
     createdAt: new Date().getTime()
   });
-
-  socket.on('disconnect',()=>{
-    console.log('CLIENTE DESCONECTADO');
-  });
+  socket.broadcast.emit('newMessage',{
+    from: 'ADMIN',
+    text: 'New USER Joined',
+    createdAt: new Date().getTime()
+   });
+  
 
   socket.on('createMessage',(message)=>{
     console.log('Create Message', message);
@@ -35,22 +35,12 @@ io.on('connection',(socket)=>{
       createdAt: new Date().getTime()
     });
   });
-/*
-  socket.emit('newEmail',{
-    from: 'chico@kbrow.com',
-    text: 'tomando Flat White',
-    createdAt: 12345
+
+   socket.on('disconnect',()=>{
+    console.log('CLIENTE DESCONECTADO');
   });
-*/  
-  /*
-  socket.on('createEmail',(newEmail)=>{
-    console.log('createEmail');
-    console.log(newEmail);
-  });
-*/
 
 });
-
 
 server.listen(port, ()=>{
   console.log(`Server is up on ${port}`);
