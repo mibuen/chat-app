4191cbb7bde7adfa25d6847d703aa0675a5b4d58
Manuel Segovia <msegovia@mac.com>
2019-10-28 08:07:07 -0400

    with moment and formated time message

--- public/js/index.js
+++ public/js/index.js
@@ -6,9 +6,10 @@ socket.on('connect', () => {
   console.log('CONNECTED TO SERVER');
 });
 socket.on('newMessage', message => {
+  const formattedTime = moment(message.createdAt).format('h:mm a');
   console.log('New Message', message);
   const li = jQuery('<li></li>');
-  li.text(`${message.from}: ${message.text}`);
+  li.text(`${message.from} ${formattedTime}: ${message.text}`);
   jQuery('#messages').append(li);
 });
 
@@ -18,13 +19,16 @@ socket.on('disconnect', () => {
 
 jQuery('#message-form').on('submit', e => {
   e.preventDefault();
+  const messageTextBox = jQuery('[name=message]');
   socket.emit(
     'createMessage',
     {
       from: 'USER',
-      text: jQuery('[name=message] ').val()
+      text: messageTextBox.val()
     },
-    () => {}
+    () => {
+      messageTextBox.val('');
+    }
   );
 });
 
@@ -32,23 +36,27 @@ locationButton.on('click', () => {
   if (!navigator.geolocation) {
     return alert('Geolocation not supported by your browser');
   }
+  locationButton.attr('disabled', 'disabled').text('Sending location...');
   navigator.geolocation.getCurrentPosition(
     position => {
+      locationButton.removeAttr('disabled').text('Send location');
       socket.emit('createLocationMessage', {
         latitude: position.coords.latitude,
         longitude: position.coords.longitude
       });
     },
     () => {
+      locationButton.removeAttr('disabled').text('Send location');
       alert('unable to Fetch current position');
     }
   );
 });
 
 socket.on('newLocationMessage', message => {
+  const formattedTime = moment(message.createdAt).format('h:mm a');
   const li = jQuery('<li></li>');
   const a = jQuery('<a target="_blank">My current location</a>');
-  li.text(`${message.from}: `);
+  li.text(`${message.from} ${formattedTime}: `);
   a.attr('href', message.url);
   li.append(a);
   jQuery('#messages').append(li);
